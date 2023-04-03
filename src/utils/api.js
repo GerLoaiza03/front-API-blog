e19c54e3878e92aedf0db605ccd3a8cc1785b1f4
GerLoaiza33 <galoaiza@uninorte.edu.co>
2023-04-02 20:49:42 -0500

    change

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 import config from '../config.js';
 
 export default axios.create({
-  baseURL: config.baseURL_API
+  baseURL: 'https://back-api-blog-production.up.railway.app/'
   //baseURL: 'http://localhost:5000'  //Con proxy en package.json, "proxy": "http://localhost:5000/"
 });
